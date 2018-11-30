1d5a9f75e70a8b6f0b422490bd6d2203510e0f0d
ilya <tihilv@mail.ru>
2018-11-30 09:59:29 +0100

    Harmony tests

--- Vkm.Console/Program.cs
+++ Vkm.Console/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using Harmony;
+using Harmony.DeviceWrappers;
 using Vkm.Intercom;
+using WebSocketSharp;
 
 namespace Vkm.Console
 {
@@ -18,9 +23,31 @@ namespace Vkm.Console
             Application.Run(new Form1());
         }
 
-        public static void Test()
+        public static async void Test()
         {
-            
+            DiscoveryService Service = new DiscoveryService();
+            Service.HubFound += async (sender, e) => {
+                // stop discovery once we've found one hub
+                //Service.StopDiscovery();
+
+                Hub Harmony = new Hub(e.HubInfo);
+                await Harmony.ConnectAsync(DeviceID.GetDeviceDefault());
+                await Harmony.SyncConfigurationAsync();
+
+                // start activity watch tv
+                Activity WatchTV = Harmony.GetActivityByLabel("Watch TV");
+                //await Harmony.StartActivity(WatchTV);
+                await Harmony.EndActivity();
+
+                // tune to ESPN
+                await Harmony.ChangeChannel("206");
+                PlaybackWrapper PlaybackControls = new PlaybackWrapper(WatchTV);
+
+                // wait for channel to switch, and pause
+                await Task.Delay(1000);
+                await Harmony.PressButtonAsync(PlaybackControls.Pause);
+            };
+            Service.StartDiscovery();
         }
     }
 }
